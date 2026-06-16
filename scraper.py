3fd7fdf897cfaff05dda138faeda4f509d97228e
Inzicv <142996930+Inzicv@users.noreply.github.com>
2026-06-16 23:10:04 +0200

    Add Firecrawl integration to scraper.py

--- scraper.py
+++ scraper.py
@@ -1,1 +1,12 @@
+from firecrawl import FirecrawlApp
+import os
 
+SEARCH_URL = "https://z-lib.fm/s/?yearFrom=2026&languages%5B%5D=french&extensions%5B%5D=EPUB&order=date"
+
+app = FirecrawlApp(
+    api_key=os.environ["FIRECRAWL_API_KEY"]
+)
+
+result = app.scrape_url(SEARCH_URL)
+
+print(result)
